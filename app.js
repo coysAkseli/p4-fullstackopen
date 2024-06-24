45fd7b813bf8067a096c185d11c9661d34b0e3e4
Akseli <akseliforma@gmail.com>
2024-06-24 11:39:09 +0300

    pc

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb+srv://akseliforma:safeahhpassword@cluster0.fhp6vln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
