e5aa311e0edd16a88d55831cb79444ced48360be
Akseli <akseliforma@gmail.com>
2024-06-26 18:06:05 +0300

    4.11 done

--- models/blog.js
+++ models/blog.js
@@ -4,11 +4,13 @@ const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: { type: Number, default: 0 }
 })
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
         delete returnedObject.__v
     }
 })
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -16,6 +17,38 @@ test('blogs are returned as json', async () => {
 test('app returns correct amount of blog posts', async () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, 2)
+})
+
+test(`identifier property is called 'id', not '_id'`, async () => {
+    const testBlog = new blog({ title: 'title', author: 'author', url: 'url', likes: 99})
+    const savedBlog = await testBlog.save()
+    const jsonBlog = savedBlog.toJSON()
+    assert.strictEqual(jsonBlog.hasOwnProperty('id'), true, 'blog HAS id property')
+    assert.strictEqual(jsonBlog.hasOwnProperty('_id'), false, 'blog does NOT have _id property')    
+    await blog.findByIdAndDelete(savedBlog._id)
+})
+
+test('HHTP POST request creates a new blog post', async () => {
+    const response = await api.get('/api/blogs')
+    const lengthBeforePost = response.body.length
+
+    const testBlogPost = new blog({ title: 'tittttle', author: 'authorrr', url: 'urlll', likes: 999})
+    const savedNewBlog = await testBlogPost.save()
+    
+    const res = await api.get('/api/blogs')
+    const lengthAfterPost = res.body.length
+
+    assert.strictEqual(lengthAfterPost-lengthBeforePost, 1)
+
+    await blog.findByIdAndDelete(savedNewBlog._id)
+})
+
+test('likes are set to 0 if \'likes\' property is missing', async () => {
+    const testLikesBlog = new blog({title: 'titleLikesTest', author: 'authorLikesTesties', url: 'urlLikesTest' })
+    const savedLikesTest = await testLikesBlog.save()
+    
+    assert.strictEqual(savedLikesTest.likes, 0)
+    await blog.findByIdAndDelete(savedLikesTest._id)
 })
 
 after(async () => {
