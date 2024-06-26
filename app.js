6cef45933e7b914d03f108fcb72bca16c156f554
Akseli <akseliforma@gmail.com>
2024-06-26 14:33:29 +0300

    create test environment, create config file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
-const mongoUrl = 'mongodb+srv://akseliforma:safeahhpassword@cluster0.fhp6vln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
+//const mongoUrl = 'mongodb+srv://akseliforma:safeahhpassword@cluster0.fhp6vln.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
