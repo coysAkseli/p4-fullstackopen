0e9979689a9fc0c0bef578773d05311d17e95814
Akseli <akseliforma@gmail.com>
2024-07-01 14:20:10 +0300

    4.19 done

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const mongoose = require('mongoose')
 
 //const mongoUrl = 'mongodb+srv://akseliforma:safeahhpassword@cluster0.fhp6vln.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0'
@@ -11,7 +14,9 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
